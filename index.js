b9c26d51966e89e2d351492c2ed3e38ea001be43
kimgahyeon <kimgostring@naver.com>
2021-03-23 22:45:08 +0900

    #1.4 Hello World with Javascript

---
+++ index.js
@@ -0,0 +1,8 @@
+// 경고 메세지
+// alert('Im working. Im JS. Im Beautiful. Im worth it.');
+
+// 콘솔에 뜸 (f12 누른 뒤 console 항목에서, 테스트에 사용 가능)
+console.log('Im working. Im JS. Im Beautiful. Im worth it.');
+
+// 언어에 따라, 프로그램을 실행하기 위해 해당 언어를 다운받아야 할 필요가 있음
+// but JS는 브라우저에 이미 구성되어 있음, 별도의 다운로드 필요 X
