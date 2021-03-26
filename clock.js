33af108b9093a76d38f624885dd1b34df565b3c4
kimgahyeon <kimgostring@naver.com>
2021-03-26 17:31:33 +0900

    #3.1 Making a JS Clock part Two

--- clock.js
+++ clock.js
@@ -5,13 +5,18 @@ function getTime() {
     const date = new Date(); // 현재 시간 정보를 담은 객체 생성
     const minutes = date.getMinutes();
     const hours = date.getHours();
+    const seconds = date.getSeconds();
 
     // 객체 안에 텍스트 넣음(h1에 해당)
-    clockTitle.innerText = `${hours}:${minutes}`;
+    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}` // 
+        + `:${minutes < 10 ? `0${minutes}` : minutes}`
+        + `:${seconds < 10 ? `0${seconds}` : seconds}`;
 }
 
 function init() {
-    getTime(); // 작동을 위해서는 f5를 눌러주어야만 함
+    getTime(); // 처음 한 번은 setInterval로 실행 X
+    setInterval(getTime, 1000); // 실행시킬 함수, 실행할 시간 간격 (ms)
+    // 이렇게 되면 실제 시간과는 약 1초가 다를 수 있는데 ㅠㅠ 어떻게 처리해야 할까? 
 }
 
 init();
