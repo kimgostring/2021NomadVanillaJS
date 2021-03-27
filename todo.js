508d72cd1f46b28d92ab9c7359e4d92b9266ab4b
kimgahyeon <kimgostring@naver.com>
2021-03-27 21:14:36 +0900

    #3.5 Making a To Do List part Two

--- todo.js
+++ todo.js
@@ -8,15 +8,23 @@ const todoForm = document.querySelector(".js-todoForm"),
 
 const TODOS_LS = "todos";
 
+const todos = [];
+
+function saveTodos() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(todos));
+}
+
 function paintTodo(text) {
     // console.log(text);
     const li = document.createElement("li"); // HTML에 무언가를 생성할 때
-    
+
     // li 안에 넣을 요소 생성
     const span = document.createElement("span");
     span.innerText = text; // 입력한 todo
     const delBtn = document.createElement("button");
     delBtn.innerText = "❌"; // 삭제 버튼 
+
+    const newID = todos.length + 1 ;
 
     // li 안에 순서대로 요소 추가
     li.appendChild(span); // li의 자식으로 span 추가
@@ -24,6 +32,17 @@ function paintTodo(text) {
 
     // todoList에 li 추가 
     todoList.appendChild(li);
+
+    // li에 id 부여, 버튼 클릭 시 어떤 li 지울지 확인하기 위해
+    li.id = newID;
+
+    const todoObj = {
+        text: text,
+        id: newID // 1부터 시작해서, 몇 번째 todo인지 기록
+    };
+
+    todos.push(todoObj);
+    saveTodos(); // 값을 push한 뒤에 local storage에 저장해야 함
 }
 
 function handleSubmitTodo(event) {
@@ -34,10 +53,16 @@ function handleSubmitTodo(event) {
 }
 
 function loadTodos() {
-    const todos = localStorage.getItem(TODOS_LS);
+    const loadTodos = localStorage.getItem(TODOS_LS);
 
-    if (todos !== null) { // todo에 뭔가 있을 때만 보여주면 됨
-        
+    if (loadTodos !== null) { // todo에 뭔가 있을 때만 보여주면 됨
+        // console.log(loadTodos);
+        const parsedTodos = JSON.parse(loadTodos); // 가져온 string을 object로 변환
+        // console.log(parsedTodos);
+
+        parsedTodos.forEach(function(todo) { // object의 원소를 순서대로 하나씩 출력
+            paintTodo(todo.text);
+        });
     }
 }
 
