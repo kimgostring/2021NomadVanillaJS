d78cb00f52ed8a65a3b540a55aeba2664fbcccee
kimgahyeon <kimgostring@naver.com>
2021-03-27 16:51:42 +0900

    #3.3 Saving the User Name part Two

--- greeting.js
+++ greeting.js
@@ -6,8 +6,25 @@ const form = document.querySelector(".js-form"),
 const USER_LS = "currentUser", // LS = local storage
     SHOWING_CN = "showing"; // CN = class name
 
+function saveName(text) {
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+    event.preventDefault(); // 기본 동작을 막기 위한 함수
+    const currentValue = input.value; // 네모 상자 안에 있는 값을 가져옴
+    // console.log(currentValue);
+    paintGreeting(currentValue); 
+    saveName(currentValue);
+}
+
+function askForName() {
+    form.classList.add(SHOWING_CN); // form 보여줌
+    form.addEventListener("submit", handleSubmit);
+}
+
 function paintGreeting(text) {
-    // form.classList.remove(SHOWING_CN); // 다음 시간에 form에 showing을 추가할 듯
+    form.classList.remove(SHOWING_CN); // 다음 시간에 form에 showing을 추가할 듯
     greeting.classList.add(SHOWING_CN);
     greeting.innerText = `Hello ${text}`;
 }
@@ -15,7 +32,7 @@ function paintGreeting(text) {
 function loadName() {
     const currentUser = localStorage.getItem(USER_LS);
     if (currentUser === null) { // 이름 없는 경우
-        
+        askForName();
     } else { // 이름 있는 경우
         paintGreeting(currentUser);
     }
